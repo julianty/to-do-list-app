e8c1c277c80e6da58c8555e85fe5bd60dffc223b
julianty <alexanderjulianty@gmail.com>
2022-06-08 10:15:14 -0700

    Adds minimal logo to webpage

--- src/generateDOM.js
+++ src/generateDOM.js
@@ -1,3 +1,5 @@
+import ToDoLogo from './ToDoLogo.png';
+
 export default function generateDOM() {
   function createElement(type, className=null) {
     const element = document.createElement(type);
@@ -14,9 +16,11 @@ export default function generateDOM() {
 
   // Create first children of main containers
   const ProjectContainer = createElement('div', 'project-container');
+  const LogoContainer = createElement('div', 'logo-container');
 
   // Add children to main containers
   Content.appendChild(ProjectContainer);
+  Header.appendChild(LogoContainer);
 
   // Add the elements to the body
   function addToBody(elementList) {
@@ -29,4 +33,13 @@ export default function generateDOM() {
   }
 
   addToBody([Header, Sidebar, Content]);
+
+  // Create Header
+  const Logo = new Image();
+  Logo.src = ToDoLogo;
+  Logo.classList.add('logo');
+  LogoContainer.appendChild(Logo);
+  const LogoText = createElement('p', 'logo-text');
+  LogoText.textContent = 'To Do List';
+  LogoContainer.appendChild(LogoText);
 }
--- src/index.js
+++ src/index.js
@@ -5,48 +5,9 @@ import DOMController from './DOMController';
 
 generateDOM();
 
-// const DOMController =  (() => {
-//   const ProjectContainer = document.querySelector('div.project-container');
-//   const ProjectList = [];
-
-//   function createElement(type, className=null) {
-//     const element = document.createElement(type);
-//     if (className) {
-//       element.classList.add(className)
-//     }
-//     return element
-//   }
-
-//   function listProjects() {
-//     ProjectList.forEach(Project => console.log(Project.name));
-//   }
-
-//   function addToDOM(Project) {
-//     ProjectList.push(Project);
-//     const NewProject = createElement('div', 'project');
-//     const ProjectName = createElement('h4', 'project-title');
-//     ProjectName.textContent = Project.name;
-//     const TaskList = createElement('ul', 'task-list');
-//     Project.taskList.forEach(task => {
-//       const TaskUL = createElement('li', 'task');
-//       TaskUL.textContent = task.title;
-//       TaskList.appendChild(TaskUL);
-//     })
-//     NewProject.appendChild(ProjectName);
-//     NewProject.appendChild(TaskList);
-//     ProjectContainer.appendChild(NewProject);
-//   }
-//   return {
-//     listProjects,
-//     addToDOM,
-//   }
-// })();
-
 
 let Project = createProject('firstProject');
 Project.addTask('Task 1');
 Project.addTask('Task 2')
-// Project.printTasks();
-// Project.addToDOM();
 DOMController.addToDOM(Project);
 DOMController.listProjects();
